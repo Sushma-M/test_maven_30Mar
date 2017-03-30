53a301d151cbefc34bc57dc4639177e5c42832fe
root <root@6afd3608e0b0>
2017-03-30 05:16:07 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "test_maven_30Mar",
+  "homePage" : "Main",
+  "name" : "test_maven_30Mar",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
